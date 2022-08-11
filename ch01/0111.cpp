68b67d481d286b1f65163274a3a65cab75f17a4c
Toshinori Sugita <s.toshi0607@gmail.com>
2022-08-11 09:56:20 +0900

    0111

---
+++ ch01/0111.cpp
@@ -0,0 +1,20 @@
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+
+    cout << "the four arithmetical operations of x and y\n";
+
+    cout << "input x and y: ";
+    cin >> x >> y;
+
+    cout << "x + y is " << x + y << "\n";
+    cout << "x - y is " << x - y << "\n";
+    cout << "x * y is " << x * y << "\n";
+    cout << "x / y is " << x / y << "\n";
+    cout << "x % y is " << x % y << "\n";
+}
