d17022817b93154c3beda2dfd524507668b70d8d
Toshinori Sugita <s.toshi0607@gmail.com>
2022-08-11 09:43:44 +0900

    0108

---
+++ ch01/0108.cpp
@@ -0,0 +1,14 @@
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+
+    cout << "x is " << x  << "\n";
+    cout << "y is " << y << "\n";
+    cout << "sum is " << x + y << "\n";
+    cout << "average is " << (x + y) / 2 << "\n";
+}
